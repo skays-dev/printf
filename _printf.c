c4cab4d401436a17655b4f43e0d9a03a9a01605a
aymane taoufyq <skays.dev@gmail.com>
2023-08-22 15:09:40 +0100

    test

--- _printf.c
+++ _printf.c
@@ -1,125 +1,46 @@
-#include "main.h"
 #include <stdarg.h>
-#include <stdio.h>
 #include <unistd.h>
-#include <limits.h>
-
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
-
-int handle_string(char *str)
-{
-    int i = 0;
-    if (str == NULL)
-    {
-        handle_string('(null)');
-        return (i);
-    }
-    while (str[i])
-    {
-        _putchar(str[i]);
-        i++;
-    }
-    return (i);
-}
-
-int len_num(int num)
-{
-    if (num == 0)
-        return (0);
-    return (1 + len_num(num / 10));
-}
-
-int handle_number(int num)
-{
-    if (num == -214783648)
-    {
-        _putchar('-');
-        _putchar('2');
-        handle_number(14783648);
-        return 1;
-    }
-    else if (num < 0)
-    {
-        _putchar('-');
-        num = -num;
-    }
-
-    if (num >= 10)
-    {
-        handle_number(num / 10);
-        handle_number(num % 10);
-    }
-    else
-    {
-        _putchar(num + '0');
-    }
-    return 1;
-}
 
 int _printf(const char *format, ...)
 {
     va_list args;
     va_start(args, format);
-    int count = 0;
 
-    if (!format || !format[0])
-    {
-        return (-1);
-    }
+    int count = 0; // To keep track of the number of characters printed
 
-    while (*format)
+    while (*format != '\0')
     {
         if (*format == '%')
         {
-            format++; // Move past '%'
-            switch (*format)
+            format++; // Move past the '%'
+
+            if (*format == 'c') // Handle character specifier
             {
-            case 'c': // %c
-                _putchar(va_arg(args, int));
+                char c = va_arg(args, int);
+                write(1, &c, 1);
                 count++;
-                break;
-            case 's': // %s
-            {
-                char *str = va_arg(args, char *);
-                count += handle_string(str);
-                break;
             }
-            case '%': // %%
-                _putchar('%');
-                count++;
-                break;
-            case 'd': // %d or %i
-            case 'i':
+            else if (*format == 's') // Handle string specifier
             {
-                int number = va_arg(args, int);
-                if (number < 0)
+                char *s = va_arg(args, char *);
+                for (int i = 0; s[i] != '\0'; i++)
                 {
-                    _putchar('-');
-                    number = -number;
+                    write(1, &s[i], 1);
+                    count++;
                 }
-                count += len_num(number);
-                handle_number(number);
             }
-            break;
-            default: // %unknown
-                _putchar(*format);
-                count += 2;
-                break;
+            else if (*format == '%') // Handle '%' specifier
+            {
+                write(1, "%", 1);
+                count++;
             }
         }
         else
         {
-            _putchar("%");
+            write(1, format, 1);
             count++;
-            if (*format)
-            {
-                _putchar(*format);
-                count++;
-            }
         }
+
         format++;
     }
 
