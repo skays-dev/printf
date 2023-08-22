2996736d913984576b09d5983734aaf7b2e3bcb6
aymane taoufyq <skays.dev@gmail.com>
2023-08-22 14:31:14 +0100

    goo

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+int printf(const char *format, ...);
+
+#endif /* MAIN_H */
