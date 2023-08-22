5cfdc4f80e36ee09248d610efc346f82bd9dfb0f
aymane taoufyq <skays.dev@gmail.com>
2023-08-22 21:31:30 +0100

    on fire

--- _printf.c
+++ _printf.c
@@ -1,45 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
 
-/**
- * _printf - Custom implementation of the printf function
- * @format: The format string containing format specifiers
- * @...: The variable number of arguments to format and print
- * Return: The total number of characters printed
- *
- * This function implements a subset of the printf function's functionality.
- * It processes the format string and any additional arguments provided
- * using the provided format specifiers. It supports the following format
- * specifiers: %c, %s, %S, %%, %d, and %i. Any other characters are printed
- * as-is. The function returns the total number of characters printed.
- */
-int _printf(const char *format, ...)
-{
-int count = 0;
-int i = 0;
-va_list args;
-
-va_start(args, format);
-
-while (format && format[i])
-{
-if (format[i] == '%')
-{
-i++;
-count = swapShift(args, count, &i, format);
-}
-else
-{
-_putchar(format[i]);
-count++;
-}
-i++;
-}
-
-va_end(args);
-
-return (count);
-}
 
 /**
  * swapShift - Process a conversion specifier in printf format
@@ -89,5 +50,45 @@ _putchar(str[*i]);
 count += 2;
 break;
 }
+return (count);
+}
+
+/**
+ * _printf - Custom implementation of the printf function
+ * @format: The format string containing format specifiers
+ * @...: The variable number of arguments to format and print
+ * Return: The total number of characters printed
+ *
+ * This function implements a subset of the printf function's functionality.
+ * It processes the format string and any additional arguments provided
+ * using the provided format specifiers. It supports the following format
+ * specifiers: %c, %s, %S, %%, %d, and %i. Any other characters are printed
+ * as-is. The function returns the total number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+int count = 0;
+int i = 0;
+va_list args;
+
+va_start(args, format);
+
+while (format && format[i])
+{
+if (format[i] == '%')
+{
+i++;
+count = swapShift(args, count, &i, format);
+}
+else
+{
+_putchar(format[i]);
+count++;
+}
+i++;
+}
+
+va_end(args);
+
 return (count);
 }
