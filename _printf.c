54f8540099dfe5e231387391027304e682f0cf87
aymane taoufyq <skays.dev@gmail.com>
2023-08-22 23:50:49 +0100

    on fire

--- _printf.c
+++ _printf.c
@@ -2,6 +2,66 @@
 #include <stdarg.h>
 
 /**
+ * handle_conversion - Handles a specific conversion specifier in printf format
+ * @args: Args list for values
+ * @count: Curr. char count (updated)
+ * @specifier: The conversion specifier to handle
+ * Return: Updated count after processing specifier
+ *
+ * This function processes a specific conversion specifier in printf format,
+ * extracting data from `args`, formatting, and printing it based on the
+ * provided conversion specifier. It supports various conversion specifiers
+ * including 'u', 'o', 'x', 'X', 'c', 's', 'S', 'p', '%', 'd', 'i', and 'b'.
+ * The function returns the updated character count after processing.
+ */
+int handle_conversion(va_list args, int count, char specifier)
+{
+switch (specifier)
+{
+case 'u':
+count = handle_unsigned(args, count);
+break;
+case 'o':
+count = handle_octal(args, count);
+break;
+case 'x':
+count = handle_hex_lower(args, count);
+break;
+case 'X':
+count = handle_hex_upper(args, count);
+break;
+case 'c':
+_putchar(va_arg(args, int));
+count++;
+break;
+case 's':
+case 'S':
+count += handle_string(args, count);
+break;
+case 'p':
+count += handle_pointer(args, count);
+break;
+case '%':
+_putchar('%');
+count++;
+break;
+case 'd':
+case 'i':
+count = handle_decimal(args, count);
+break;
+case 'b':
+count = handle_binary(args, count);
+break;
+default:
+_putchar(str[*i - 1]);
+_putchar(str[*i]);
+count += 2;
+break;
+}
+return (count);
+}
+
+/**
  * swapShift - Process a conversion specifier in printf format
  * @args: Args list for values
  * @count: Curr. char count (updated)
@@ -10,63 +70,16 @@
  * Return: Updated count after processing specifier
  *
  * This function handles a specifier in printf format. Using
- * char at index `i` in `str`, it extracts data from `args`,
- * formats, and prints it. Updates `count` to track printed.
+ * char at index `i` in `str`, it extracts the conversion specifier,
+ * and delegates the processing to the appropriate handler using
+ * the `handle_conversion` function. Updates `count` to track printed.
  */
-
-int handle_conversion(va_list args, int count, char specifier)
-{
-    switch (specifier)
-    {
-        case 'u':
-            count = handle_unsigned(args, count);
-            break;
-        case 'o':
-            count = handle_octal(args, count);
-            break;
-        case 'x':
-            count = handle_hex_lower(args, count);
-            break;
-        case 'X':
-            count = handle_hex_upper(args, count);
-            break;
-        case 'c':
-            _putchar(va_arg(args, int));
-            count++;
-            break;
-        case 's':
-        case 'S':
-            count += handle_string(args, count);
-            break;
-        case 'p':
-            count += handle_pointer(args, count);
-            break;
-        case '%':
-            _putchar('%');
-            count++;
-            break;
-        case 'd':
-        case 'i':
-            count = handle_decimal(args, count);
-            break;
-        case 'b':
-            count = handle_binary(args, count);
-            break;
-        default:
-            _putchar(str[*i - 1]);
-            _putchar(str[*i]);
-            count += 2;
-            break;
-    }
-    return count;
-}
-
 int swapShift(va_list args, int count, int *i, const char *str)
 {
-    (*i)++;
-    char specifier = str[*i];
-    count = handle_conversion(args, count, specifier);
-    return count;
+(*i)++;
+char specifier = str[*i];
+count = handle_conversion(args, count, specifier);
+return count;
 }
 
 /**
